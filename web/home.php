c22f6eebc8a7c4c149801c6a68bf0872abaece9a
LTVlad <vlad.lupu12@yahoo.com>
2017-06-03 16:49:47 +0300

    web update

--- web/home.php
+++ web/home.php
@@ -7,10 +7,14 @@
 		header('location:../index.php');
     	exit;
     }
+    if(isset($_GET["page"]))
+    	$_SESSION['page']=$_GET["page"];
+    else
+    	$_SESSION['page']=0;
 
-    $_SESSION['page']=$_GET["page"];
+    echo $_SESSION['page'];
 
-    if (isset($_POST['create'])) {
+    if (isset($_POST['create-closet'])) {
     	if(isset($_POST['closet-name']))
     	{
     		$id=$_SESSION['id'];
@@ -29,11 +33,11 @@
 	        header('LOCATION:home.php?msg=failed-name');
     }
 
-    elseif (isset($_POST['edit']))
+    elseif (isset($_POST['edit-closet']))
     {
     	if(isset($_POST['closet-name']))
     	{
-    		$id=$_SESSION['id'];
+    		$id=$_SESSION['page']+1;
     		$closet_name=$_POST['closet-name'];
     		$sql = 'BEGIN closets_package.edit_closet(:v_id, :v_closet_name, :v_output); END;';
     		$stmt = oci_parse($conn,$sql);
@@ -43,8 +47,73 @@
 		    oci_execute($stmt);
     	}
     	
-	    if ($closet_success>0)
-	        header('Location:home.php?page='.$closet_success);
+	    if ($closet_success==0)
+	        header('Location:home.php?page='.$id);
+	    else
+	        header('LOCATION:home.php?msg=failed-name');
+    }
+	elseif (isset($_POST['delete-closet']))
+	{
+		$id=$_SESSION['page']+1;
+    	$closet_name=$_POST['closet-name'];
+    	$sql = 'BEGIN closets_package.delete_closet(:v_id); END;';
+    	$stmt = oci_parse($conn,$sql);
+		oci_bind_by_name($stmt,":v_id",$id,32);
+		oci_execute($stmt);
+	}
+
+    if (isset($_POST['create-drawer'])) {
+    	if(isset($_POST['drawer-name']))
+    	{
+
+    		$id=1;
+    		$drawer_name=$_POST['drawer-name'];
+    		$sql = 'BEGIN drawers_package.create_drawer(:v_closet_id, :v_drawer_name, :v_locked, :v_password, :v_output); END;';
+    		$stmt = oci_parse($conn,$sql);
+		   	oci_bind_by_name($stmt,":v_closet_id",$id,32);
+		    oci_bind_by_name($stmt,":v_drawer_name",$drawer_name,32);
+    		if(isset($_POST['chkpass']))
+    			$locked=1;
+    		else
+    			$locked=0;
+
+    		oci_bind_by_name($stmt,":v_locked",$locked,32);
+
+    		if(isset($_POST['password']))
+    			{
+    				$password=$_POST['password'];
+    				oci_bind_by_name($stmt,":v_password",$password,32);
+    			}
+    		else
+    			$password="das";
+    		
+    		oci_bind_by_name($stmt,":v_output",$drawer_success,32);
+    		oci_execute($stmt);
+
+		    if ($drawer_success>0)
+		        header('Location:home.php?page='.$drawer_success);
+		    else
+		        header('LOCATION:home.php?msg=failed-name');
+		}
+    }
+    
+
+    elseif (isset($_POST['edit-drawer']))
+    {
+    	if(isset($_POST['drawer-name']))
+    	{
+    		$id=$_SESSION['id'];
+    		$drawer_name=$_POST['drawer-name'];
+    		$sql = 'BEGIN drawers_package.edit_closet(:v_id, :v_drawer_name, :v_output); END;';
+    		$stmt = oci_parse($conn,$sql);
+		    oci_bind_by_name($stmt,":v_id",$id,32);
+		    oci_bind_by_name($stmt,":v_drawer_name",$drawer_name,32);
+		    oci_bind_by_name($stmt,":v_output",$drawer_success,32);
+		    oci_execute($stmt);
+    	}
+    	
+	    if ($drawer_success>0)
+	        header('Location:home.php?page='.$drawer_success);
 	    else
 	        header('LOCATION:home.php?msg=failed-name');
     }
@@ -85,15 +154,15 @@
 
 		<form class="form home" action="home.php" method="post">
 		
-			<button type="submit" name="create">Create</button>
+			<button type="submit" name="create-closet">Create</button>
 			<input type="text" name="closet-name" placeholder="Closet Name">
 
-			<button type="submit" name="edit">Edit</button>
-			<button type="submit" name="delete">Delete</button>
+			<button type="submit" name="edit-closet">Edit</button>
+			<button type="submit" name="delete-closet">Delete</button>
 			
 			<?php
-                if (isset($_GET["msg"]) && $_GET["msg"] == 'success') {
-                echo '<p class="message success">Closet created!</p>';
+                if (isset($_GET["page"]) && $_GET["page"] > '0') {
+                echo '<p class="message success">Closet created or edited!</p>';
                 }
 
                 if (isset($_GET["msg"]) && $_GET["msg"] == 'failed-name') {
@@ -101,7 +170,7 @@
                 }
             ?>
 		</form>
-		<?php $_SESSION['page']=$_GET["page"]-1; echo '<a href="home.php?page='.$_SESSION['page'].'">';  ?> <img class="arrow-left" src="images/arrow-left.png" alt="Arrow left"></a>
+		<?php $_SESSION['page']=$_GET["page"]-1; echo '<a href="home.php?page='.$_SESSION['page'].'">'; ?> <img class="arrow-left" src="images/arrow-left.png" alt="Arrow left"></a>
 	        <table>
 			  <tr>
 			    <th colspan="4"></th>
@@ -128,9 +197,10 @@
 			    <td></td>
 			  </tr>
 			</table>
-        <a href="#"><img class="arrow-right" src="images/arrow-right.png" alt="Arrow right"></a>
+        <?php $_SESSION['page']=$_GET["page"]+1; echo '<a href="home.php?page='.$_SESSION['page'].'">';  ?><img class="arrow-right" src="images/arrow-right.png" alt="Arrow right"></a>
 		
-		<form class="form home" action="#">
+		<form class="form home" action="home.php" method="post">
+		<button type="submit" name="create-drawer">Create</button>
 			 <select name="drawers" form="drawer-form">
 				  <option value="drawer1">Drawer 1</option>
 				  <option value="drawer2">Drawer 2</option>
@@ -143,10 +213,14 @@
 				  <option value="drawer9">Drawer 9</option>
 				  <option value="drawer10">Drawer 10</option>
 			</select>
-		<input type="text" name="fname" placeholder="Drawer Name">
+		<input type="text" name="drawer-name" placeholder="Drawer Name">
 
-		<button type="submit" name="submit">Edit</button>
-		<button type="submit" name="submit">Delete</button>
+		<button type="submit" name="edit-drawer">Edit</button>
+		<button type="submit" name="delete-drawer">Delete</button>
+
+		<input type="checkbox" placeholder="PIN" class="pin" name="chkpass"/>
+   		<input type="text" placeholder="PIN Code" class="pincode" name="password"/>
+
 		</form>
 
 	</main>
